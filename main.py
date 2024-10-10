d94ec0915ac140ab580bd8b03822cb20972ea9b5
Ali omidi <aliakbaromidyy@gmail.com>
2024-10-11 00:41:09 +0330

    Start project

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#start
+
+import telebot
+
+bot = telebot.TeleBot('7392506579:AAGNVj-Qfw-by0_uM3sz4SOiCJ3GpBHEfRo')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "به ربات دانلودر فیلم خوش اومدی")
+
+
+bot.polling()
