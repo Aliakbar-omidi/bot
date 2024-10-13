6dfd0667144ac6abf411ac0f67c01728e5b77aa8
Ali omidi <aliakbaromidyy@gmail.com>
2024-10-13 12:19:49 +0330

    Add new decorators

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import telebot
+import re
 
 bot = telebot.TeleBot('7392506579:AAGNVj-Qfw-by0_uM3sz4SOiCJ3GpBHEfRo')
 
@@ -7,18 +8,25 @@ bot = telebot.TeleBot('7392506579:AAGNVj-Qfw-by0_uM3sz4SOiCJ3GpBHEfRo')
 def send_welcome(message):
     bot.reply_to(message, " سلام به ربات دانلودر فیلم خوش اومدی ")
 
-# send message for text
-@bot.message_handler(func=lambda message: True)
-def handle_text_message(message):
-    bot.reply_to(message, "لطفا از دستورات داخل منو استفاده کنید")
 
-
-# Handles all sent documents and audio files
-@bot.message_handler(content_types=['audio','document'])
+# Handles all sent documents, audio files, and voice messages
+@bot.message_handler(content_types=['audio','document','voice'])
 def message_for_file(message):
     if message.audio:
         bot.reply_to(message, 'This is an audio file')
     elif message.document:
         bot.reply_to(message, 'This is a document file')
+    elif message.voice:
+        bot.reply_to(message, 'This is voice')
+
+
+# Handles all text message that match the regular expression
+@bot.message_handler(func=lambda message: True)
+def message_all(message):
+    if re.match(r"^[A-Za-z]+$", message.text):
+        bot.reply_to(message, 'لطفاً فقط فارسی تایپ کنید.')
+
+
+# Handles all message for wich the lambda returns True
 
 bot.polling()
