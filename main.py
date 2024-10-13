314caf1fbc80a5f554305c627ba094dc6daf04c2
Ali omidi <aliakbaromidyy@gmail.com>
2024-10-13 10:50:12 +0330

    Add additional functions

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ bot = telebot.TeleBot('7392506579:AAGNVj-Qfw-by0_uM3sz4SOiCJ3GpBHEfRo')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "به ربات دانلودر فیلم خوش اومدی")
+    bot.reply_to(message, "لطفا از دستورات داخل منو استفاده کنید")
 
+# send message for text
+@bot.message_handler(func=lambda message: True)
+def handle_text_message(message):
+    bot.reply_to(message, "سیکتو بزن")
+
+
+# Handles all sent documents and audio files
+@bot.message_handler(content_types=['audio','document'])
+def message_for_file(message):
+    if message.audio:
+        bot.reply_to(message, 'This is an audio file')
+    elif message.document:
+        bot.reply_to(message, 'This is a document file')
 
 bot.polling()
