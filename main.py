d4004a86ada9c8f89fb673761feba3f29df5509e
Ali omidi <aliakbaromidyy@gmail.com>
2024-10-13 10:52:48 +0330

    Add additional functions

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ bot = telebot.TeleBot('7392506579:AAGNVj-Qfw-by0_uM3sz4SOiCJ3GpBHEfRo')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "لطفا از دستورات داخل منو استفاده کنید")
+    bot.reply_to(message, " سلام به ربات دانلودر فیلم خوش اومدی ")
 
 # send message for text
 @bot.message_handler(func=lambda message: True)
 def handle_text_message(message):
-    bot.reply_to(message, "سیکتو بزن")
+    bot.reply_to(message, "لطفا از دستورات داخل منو استفاده کنید")
 
 
 # Handles all sent documents and audio files
