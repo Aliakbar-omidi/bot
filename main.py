e2780398b5ca1b15c1ae81d0d2a9d3bb98447115
Ali omidi <aliakbaromidyy@gmail.com>
2024-11-03 03:25:32 +0330

    Updating and rewriting some codes

--- main.py
+++ main.py
@@ -11,50 +11,38 @@ bot = telebot.TeleBot(API_token)
 
 # start command
 User_id = []
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    if message.chat.id not in User_id:
-        User_id.append(message.chat.id)
-
-    bot.send_message(message.chat.id, " سلام لطفا اسم خودت رو بهم بگو ")
-    bot.register_next_step_handler(message, process_name)
-
-
-def process_name(message):
-    name = message.text
-    bot.send_message(message.chat.id, f"خب {name} حالا به من بگو که چند سالته؟")
-
-    bot.register_next_step_handler(message, process_age)
-
-
-def process_age(message):
-    age = message.text
-    bot.send_message(message.chat.id, f"پس {age} سالته \n خیلی ام عالی.")
-
-#################### End of command #############################
-
-
-# Creating the reply keyboard (criticism command)
 reply_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 reply_keyboard.add("btn1", "btn2", "close")
 
-@bot.message_handler(commands=['criticism'])
-def check_criticism(message):
-    bot.reply_to(message, "Check the following keyboard.", reply_markup=reply_keyboard)
-
 @bot.message_handler(func=lambda message: True)
-def check_reply_button(message):
-    if message.text == "btn1":
-        bot.reply_to(message, "button1 is pressed")
-    elif message.text == "btn2":
-        bot.reply_to(message, "button2 is pressed")
-    elif message.text == "close":
-        bot.send_message(message.chat.id, "دستور بسته شد.", reply_markup=types.ReplyKeyboardRemove())
-    elif re.match(r"^[A-za-zآ-ی0-9]+$", message.text):
-        bot.reply_to(message, f"your message is {message.text}")
+def process_command(message):
+    if message.text == '/start':
+        if message.chat.id not in User_id:
+            User_id.append(message.chat.id)
 
-#################### End of command #############################
+        bot.send_message(message.chat.id, "سلام، لطفاً اسم خودت رو بهم بگو ")
+        bot.register_next_step_handler(message, process_name)
+    elif message.text == '/criticism':
+        bot.reply_to(message, "کیبورد زیر رو چک کن  و")
+        bot.send_message(message.chat.id, "بازخورد خودت رو بنویس", reply_markup=reply_keyboard)
+    else:
+        if message.text == "btn1":
+            bot.reply_to(message, "دکمه شماره 1 رو فشردید")
+        elif message.text == "btn2":
+            bot.reply_to(message, "دکمه شماره 2 رو فشردید")
+        elif message.text == "close":
+            bot.send_message(message.chat.id, "دستور بسته شد.", reply_markup=types.ReplyKeyboardRemove())
+        elif re.match(r"^[A-Za-zآ-ی0-9]+$", message.text):
+            bot.reply_to(message, f"پیام شما: {message.text}")
+
+def process_name(message):
+    name = message.text
+    bot.send_message(message.chat.id, f"خب {name} حالا به من بگو چند سالته؟")
+    bot.register_next_step_handler(message, process_age)
+
+def process_age(message):
+    age = message.text
+    bot.send_message(message.chat.id, f"پس {age} سالته. خیلی عالی!")
 
 
 # Handles all sent documents, audio files, and voice messages
@@ -66,17 +54,6 @@ def message_for_file(message):
         bot.reply_to(message, 'This is a document file')
     elif message.voice:
         bot.reply_to(message, 'This is voice')
-
-#################### End of command #############################
-
-
-# Handles all text message that match the regular expression
-@bot.message_handler(func=lambda message: True)
-def message_all(message):
-    if re.match(r"^[A-Za-z]+$", message.text):
-        bot.reply_to(message, 'لطفاً فقط فارسی تایپ کنید.')
-
-#################### End of command #############################
 
 
 # adding button for inlinekeyboard (call command)
